4e35b850beaf9d276e9ba421c910564757090307
SamuelBacelar <s.c.bacelar@gmail.com>
2020-11-12 15:21:38 -0300

    fixing webhook slack notification

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,6 +1,5 @@
-# module SlackNotifier
-#     CLIENT = Slack::Notifier.new Rails.application.credentials.slack[:token]
-#   end
+module SlackNotifier
+    CLIENT = Slack::Notifier.new Rails.application.credentials.slack[:token]
+  end
 
-require 'slack-notifier'
-notifier = Slack::Notifier.new "https://hooks.slack.com/services/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"
+
